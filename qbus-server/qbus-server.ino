bf1db8e334848ee0e76ea451eec5e864691b8055
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2020-10-20 22:14:21 +0200

    Random ID

--- qbus-server/qbus-server.ino
+++ qbus-server/qbus-server.ino
@@ -1,60 +1,41 @@
-#include <ESP8266WiFi.h> // Include the Wi-Fi library
-#include <WiFiClient.h> // Include WiFi Client
-#include <ESP8266WebServer.h> // Include Web Server
-
-const char *ssid = "Qbus";
-const char *password = "__qbus__";
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 ESP8266WebServer server(80);
 
+int r = random(100, 999)
+
+const char *ssid = "Qbus" + to_string(r)
+const char *WiFiPWD = "q" + to_string(r)
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  delay(10);
-  Serial.println('\n');
+  Serial.println("");
 
-  WiFi.hostname("Qbus"); // Set hostname
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, WiFiPWD);
 
-  WiFi.softAP(ssid, password); // Start the access point
-  Serial.print("Access Point \"");
-  Serial.print(ssid);
-  Serial.println("\" started");
+  delay(500);
 
-  Serial.print("IP address:\t");
+  Serial.println("IP Adress:");
   Serial.println(WiFi.softAPIP());
 
-  runServer();
+  Serial.println("[DEBUG] Begin Server")
+
+  serverSetup()
+
+  server.begin(); // Run Server
+  Serial.println("[DEBUG] Run HTTP Server");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  connectedCount();
-  server.handleClient();
+
 }
 
-void connectedCount() {
-  Serial.print("\nPlayers: ");
-  Serial.print(playerCount());
-  delay(2000);
-  connectedCount();
+void serverSetup() {
+  server.onNotFound(sNF)
 }
 
-double playerCount() {
-  return WiFi.softAPgetStationNum();
-}
-
-void runServer() {
-  server.on("/", serverRoot);
-
-  server.onNotFound(serverNF);
-
-  server.begin();
-}
-
-void serverRoot() {
-  server.send(200, "text/html", "<h1><b>Q</b>bus</h1>");
-}
-
-void serverNF() {
-  server.send(4040, "text/html", "<h1>404 Not FOund</h1><hr /><b>Q</b>bus");
+void sNF() {
+  server.send(404, "text/plain", "Error: 404 - Not Found")
 }
